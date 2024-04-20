55deb788511b4b977a7ba838bd3af18ae8a4b2f8
Brian Nguyen <nguyenbrian92620@gmail.com>
2024-04-20 14:02:11 -0700

    first commit

---
+++ backend/api/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+
+# Create your models here.
+class Profile(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    bio = models.TextField()
+    school = models.CharField(max_length=100)
+    major = models.CharField(max_length=100)
+    pfp = models.URLField()
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name="profile")
