c1ecbfc9ae3ab0607b3b78f717becfa371bcc7e1
ethanoutangoun <ethanoutangoun@gmail.com>
2024-04-20 14:48:10 -0700

    database env setup done

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -98,8 +98,12 @@ WSGI_APPLICATION = "backend.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": os.getenv("DATABASE_ENGINE"),
+        "NAME": os.getenv("DATABASE_NAME"),
+        "USER": os.getenv("DATABASE_USER"),
+        "PASSWORD": os.getenv("DATABASE_PASSWORD"),
+        "HOST": os.getenv("DATABASE_HOST"),
+        "PORT": os.getenv("DATABASE_PORT"),
     }
 }
 
