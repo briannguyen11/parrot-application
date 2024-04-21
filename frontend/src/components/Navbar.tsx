550d55be361dba1498d66d397c96c6e8586a2f48
ethanoutangoun <ethanoutangoun@gmail.com>
2024-04-20 18:22:40 -0700

    shadcn init/w custom navbar

---
+++ frontend/src/components/Navbar.tsx
@@ -0,0 +1,12 @@
+const Navbar = () => {
+  return (
+    <div className="md:px-16 px-5 py-3 flex shadow-light">
+      <div className="flex justify-center items-center gap-3">
+        <img src="../../public/icon.png" alt="logo" className="w-10 h-10" />
+        <h1 className="text-xl font-medium">SPRINT</h1>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
