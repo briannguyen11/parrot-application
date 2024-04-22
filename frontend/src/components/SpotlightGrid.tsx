9be00300bffe2ee54d02865626415394b68c2c09
ethanoutangoun <ethanoutangoun@gmail.com>
2024-04-21 21:34:04 -0700

    spotlight for big screens

--- frontend/src/components/SpotlightGrid.tsx
+++ frontend/src/components/SpotlightGrid.tsx
@@ -9,14 +9,16 @@ const SpotlightGrid = () => {
   useEffect(() => {
     const timer = setTimeout(() => {
       setLoading(false);
-    }, 1000);
+    }, 0);
 
     return () => clearTimeout(timer);
   }, []);
 
   if (loading) {
     return (
-      <div className="mt-5 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2  gap-x-10 gap-y-16">
+      <div className="mt-5 grid 4xl:grid-cols-3 xl:grid-cols-2 grid-cols-1  gap-x-10 gap-y-16">
+        <Skeleton className="aspect-spotlight rounded-3xl" />
+        <Skeleton className="aspect-spotlight rounded-3xl" />
         <Skeleton className="aspect-spotlight rounded-3xl" />
         <Skeleton className="aspect-spotlight rounded-3xl" />
         <Skeleton className="aspect-spotlight rounded-3xl" />
@@ -26,14 +28,18 @@ const SpotlightGrid = () => {
   }
 
   return (
-    <div className="mt-5 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2  gap-x-10 gap-y-16">
+    <div className="mt-5 grid 4xl:grid-cols-3 xl:grid-cols-2 grid-cols-1  gap-x-10 gap-y-16">
       <SpotlightCard
         projectName="Project Title"
-        imageUrls={["https://cdn.myportfolio.com/6771405b-f126-49e0-9c51-62863cf81a97/e2ea8b1f-f789-4c2b-adde-85fd00506d34_rw_1920.png?h=fe8eeaae3123cb8a0795b940985ed85e"]}
+        imageUrls={[
+          "https://cdn.myportfolio.com/6771405b-f126-49e0-9c51-62863cf81a97/e2ea8b1f-f789-4c2b-adde-85fd00506d34_rw_1920.png?h=fe8eeaae3123cb8a0795b940985ed85e",
+        ]}
       />
       <SpotlightCard
         projectName="Project Title"
-        imageUrls={["https://cdn.myportfolio.com/6771405b-f126-49e0-9c51-62863cf81a97/e2ea8b1f-f789-4c2b-adde-85fd00506d34_rw_1920.png?h=fe8eeaae3123cb8a0795b940985ed85e"]}
+        imageUrls={[
+          "https://cdn.myportfolio.com/6771405b-f126-49e0-9c51-62863cf81a97/e2ea8b1f-f789-4c2b-adde-85fd00506d34_rw_1920.png?h=fe8eeaae3123cb8a0795b940985ed85e",
+        ]}
       />
     </div>
   );
