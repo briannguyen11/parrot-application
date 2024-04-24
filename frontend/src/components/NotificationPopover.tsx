578f52ee6b9a663e043e9a3198b0846621f7ce61
ethanoutangoun <ethanoutangoun@gmail.com>
2024-04-24 01:06:56 -0700

    fix popover vertical alignment

--- frontend/src/components/NotificationPopover.tsx
+++ frontend/src/components/NotificationPopover.tsx
@@ -15,7 +15,7 @@ const NotificationPopover = () => {
           className="w-8 h-8 object-cover hover:cursor-pointer hover:bg-gray-200 p-1 rounded-full transition duration-300 ease-in-out"
         />
       </PopoverTrigger>
-      <PopoverContent className="md:mr-20 mr-16 mt-4">Place content for the popover here.</PopoverContent>
+      <PopoverContent className="md:mr-20 mr-16 mt-3">Place content for the popover here.</PopoverContent>
     </Popover>
   );
 };
