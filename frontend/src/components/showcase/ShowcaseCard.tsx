7e8775656098ad00e0a536c65e47085f6f83a21c
ethanoutangoun <ethanoutangoun@gmail.com>
2024-05-12 17:16:09 -0700

    preload showcase images before rendering, fixes slow initial cycle

--- frontend/src/components/showcase/ShowcaseCard.tsx
+++ frontend/src/components/showcase/ShowcaseCard.tsx
@@ -1,41 +1,53 @@
-import { useState } from "react";
+import React, { useState, useEffect } from "react";
 
-
-interface photo {
+interface Photo {
   photo: string;
-  caption : string;
-  id : number;
-  project : number;
+  caption: string;
+  id: number;
+  project: number;
 }
 
 type ShowcaseCardProps = {
   projectName: string;
-  photos: photo[]; // Define a type for imageUrls prop as an array of strings
+  photos: Photo[];
 };
 
-const ShowcaseCard: React.FC<ShowcaseCardProps> = ({
-  projectName,
-  photos,
-}) => {
-  
+const ShowcaseCard: React.FC<ShowcaseCardProps> = ({ projectName, photos }) => {
   const [photoIndex, setPhotoIndex] = useState(0);
+  const [preloadedImages, setPreloadedImages] = useState<string[]>([]);
 
-
+  useEffect(() => {
+    const preloadImages = () => {
+      const imagePromises = photos.map((photo) => {
+        return new Promise<string>((resolve) => {
+          const img = new Image();
+          img.src = photo.photo;
+          img.onload = () => {
+            resolve(photo.photo);
+          };
+        });
+      });
+      Promise.all<string>(imagePromises).then((images) =>
+        setPreloadedImages(images)
+      );
+    };
+    preloadImages();
+  }, [photos]);
 
   return (
-    <div onClick={() => setPhotoIndex((photoIndex + 1) % photos.length)} className="aspect-spotlight bg-gray-50 relative rounded-sm shadow-light hover:cursor-pointer overflow-clip hover:scale-103 transition duration-300 ease-in-out select-none">
-      {photos.length > 0 ? (
+    <div
+      onClick={() => setPhotoIndex((photoIndex + 1) % photos.length)}
+      className="aspect-spotlight bg-gray-50 relative rounded-sm shadow-light hover:cursor-pointer overflow-clip hover:scale-103 transition duration-300 ease-in-out select-none"
+    >
+      {preloadedImages.length > 0 ? (
         <img
-          src={photos[photoIndex].photo}
+          src={preloadedImages[photoIndex]}
           alt="placeholder"
           className="object-cover w-full h-full "
           draggable="false"
-          
         />
       ) : (
-        <div className="w-[100vw] h-[100vh] bg-gray-400">
-         
-        </div>
+        <div className="w-[100vw] h-[100vh] bg-gray-400"></div>
       )}
       <div className="absolute bottom-0 left-0 w-full h-full  bg-card-hover opacity-0 hover:opacity-100">
         <div className="flex items-end h-full w-full">
