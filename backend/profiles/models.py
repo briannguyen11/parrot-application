e7fc07e38a29f502a072c09564c2f57813e551b6
paulmunar <paulleroymunar@gmail.com>
2024-05-18 07:59:13 -0700

    Edited profiles model default fields

--- backend/profiles/models.py
+++ backend/profiles/models.py
@@ -7,13 +7,13 @@ class Profiles(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, related_name="profile")
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
-    school = models.CharField(max_length=100)
-    major = models.CharField(max_length=100)
-    bio = models.TextField()
-    profile_picture = models.ImageField(upload_to="profiles/profile_pics", blank=True)
-    resume = models.FileField(upload_to="profiles/resumes", blank=True)
-    linkedin = models.CharField(max_length=250)
-    github = models.CharField(max_length=250)
+    school = models.CharField(max_length=100, blank=True, default="")
+    major = models.CharField(max_length=100, blank=True, default="")
+    bio = models.TextField(blank=True, default="")
+    profile_picture = models.ImageField(upload_to="profiles/profile_pics", blank=True, null=True)
+    resume = models.FileField(upload_to="profiles/resumes", blank=True, null=True)
+    linkedin = models.CharField(max_length=250, blank=True, default="")
+    github = models.CharField(max_length=250, blank=True, default="")
 
     def __str__(self):
         return f"{self.first_name} {self.last_name}'s profile"
