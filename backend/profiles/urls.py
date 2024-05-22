88fb60af44da56db1d8302b466a3c63e4baa1ff1
Ethan Outangoun <ethanoutangoun@gmail.com>
2024-05-21 23:42:58 -0700

    took out link on accident

--- backend/profiles/urls.py
+++ backend/profiles/urls.py
@@ -4,7 +4,7 @@ from profiles.views import ProfilesViewSet, ProfileSearchViewSet
 
 router = DefaultRouter()
 router.register(r"search", ProfileSearchViewSet, basename="profile-search")
-# router.register(r"", ProfilesViewSet, basename="user-profile")
+router.register(r"", ProfilesViewSet, basename="user-profile")
 
 
 urlpatterns = router.urls
