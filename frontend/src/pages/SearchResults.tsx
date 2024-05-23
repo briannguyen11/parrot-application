e756ea3f72741440bf0650b6f757aae73426764e
Ethan Outangoun <ethanoutangoun@gmail.com>
2024-05-22 18:53:04 -0700

    improved search ui

--- frontend/src/pages/SearchResults.tsx
+++ frontend/src/pages/SearchResults.tsx
@@ -3,6 +3,7 @@ import { useLocation } from "react-router-dom";
 import { useEffect, useState } from "react";
 import api from "@/api";
 import BlankAvatar from "../assets/icons/person.svg";
+import { Skeleton } from "@/components/ui/skeleton";
 
 
 interface Profile {
@@ -13,11 +14,55 @@ interface Profile {
   profile_picture: string;
 }
 
+const renderSkeleton = () => {
+  return (
+    <>
+      <div className="mt-5 flex flex-col ">
+        <div className="flex items-start gap-4 border-b py-5">
+          <Skeleton className="h-12 w-12 rounded-full" />
+          <div>
+            <Skeleton className="h-4 w-40" />
+
+            <Skeleton className="mt-3 h-4 w-60" />
+          </div>
+        </div>
+
+        <div className="flex items-start gap-4 border-b py-5">
+          <Skeleton className="h-12 w-12 rounded-full" />
+          <div>
+            <Skeleton className="h-4 w-40" />
+
+            <Skeleton className="mt-3 h-4 w-60" />
+          </div>
+        </div>
+
+        <div className="flex items-start gap-4 border-b py-5">
+          <Skeleton className="h-12 w-12 rounded-full" />
+          <div>
+            <Skeleton className="h-4 w-40" />
+
+            <Skeleton className="mt-3 h-4 w-60" />
+          </div>
+        </div>
+
+        <div className="flex items-start gap-4 py-5">
+          <Skeleton className="h-12 w-12 rounded-full" />
+          <div>
+            <Skeleton className="h-4 w-40" />
+
+            <Skeleton className="mt-3 h-4 w-60" />
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
 
 const SearchResults = () => {
   const location = useLocation();
   const [searchQuery, setSearchQuery] = useState<string | null>(null);
   const [profiles, setProfiles] = useState([] as Profile[]);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     const params = new URLSearchParams(location.search);
@@ -26,6 +71,8 @@ const SearchResults = () => {
   }, [location.search]);
 
   useEffect(() => {
+
+    setLoading(true);
     const fetchSearchResults = async () => {
       console.log(`/api/profiles/search/?query=${searchQuery}`);
 
@@ -34,6 +81,7 @@ const SearchResults = () => {
       );
       console.log(response.data);
       setProfiles(response.data);
+      setLoading(false);
     };
 
     searchQuery && fetchSearchResults();
@@ -41,31 +89,42 @@ const SearchResults = () => {
 
   return (
     <div className="w-full pt-5 lg:pl-5">
-      <h1 className="mb-5 text-xl font-medium">Search results for '{searchQuery}'</h1>
-    
+      <h1 className="mb-5 text-xl font-medium">
+        Search results for '{searchQuery}'
+      </h1>
 
       <div className="bg-white border rounded-lg p-4">
-        <h2 className="text-lg font-medium">People</h2>
+        <h2 className="text-xl font-medium text-primary">People</h2>
+        {loading && renderSkeleton()}
+        {!loading && (
+          <div className="mt-5 flex flex-col ">
+            {profiles.map((result, index) => (
+              <div
+                key={result.id}
+                className={`flex items-start gap-4 border-b py-5 ${
+                  index === profiles.length - 1 ? "border-b-0" : ""
+                }`}
+              >
+                <img
+                  src={result.profile_picture || BlankAvatar}
+                  alt="profile"
+                  className="w-12 h-12 rounded-full"
+                />
+                <div>
+                  <h3 className="text-md font-medium">
+                    {result.first_name + " " + result.last_name}
+                  </h3>
 
-        <div className="mt-5 flex flex-col gap-8">
-          {profiles.map((result) => (
-            <div key={result.id} className="flex items-center gap-4">
-              <img
-                src={result.profile_picture || BlankAvatar}
-                alt="profile"
-                className="w-12 h-12 rounded-full"
-              />
-              <div>
-                <h3 className="text-lg font-medium">
-                  {result.first_name + " " + result.last_name}
-                </h3>
-
-                <p>{result.bio}</p>
+                  <p className="text-sm text-secondary">{result.bio}</p>
+                </div>
               </div>
-            </div>
-          ))}
-          <div></div>
-        </div>
+            ))}
+
+            {profiles.length === 0 && (
+              <p className="text-md text-secondary">No results found</p>
+            )}
+          </div>
+        )}
       </div>
     </div>
   );
