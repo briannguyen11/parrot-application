348c0548ef58b963db37e7f93e83e6b9f36829f9
Brian Nguyen <nguyenbrian92620@gmail.com>
2024-06-08 23:42:49 -0700

    working on onbaording

--- frontend/src/pages/Onboard.tsx
+++ frontend/src/pages/Onboard.tsx
@@ -3,46 +3,49 @@ import { Button } from "@/components/ui/button";
 import { useNavigate } from "react-router-dom";
 import { UserAuth } from "@/auth/AuthContext";
 import api from "../api";
-import PhotoInput from "@/components/profile/PhotoInput";
+import ProfilePictureInput from "@/components/profile/ProfilePictureInput";
+import BannerInput from "@/components/profile/BannerInput";
 
 const Onboard = () => {
   const [firstName, setFirstName] = useState<string>("");
   const [lastName, setLastName] = useState<string>("");
+  const [username, setUsername] = useState<string>("");
   const [school, setSchool] = useState<string>("");
   const [major, setMajor] = useState<string>("");
+  const [header, setHeader] = useState<string>("");
   const [bio, setBio] = useState<string>("");
   const [resume, setResume] = useState<File>();
   const [pfp, setPfp] = useState<File>();
+  const [banner, setBanner] = useState<File>();
   const [linkedin, setLinkedin] = useState<string>("");
   const [github, setGithub] = useState<string>("");
   const [slide, setSlide] = useState(1);
-
-  const navigate = useNavigate();
-
   const { setUserPfp } = UserAuth();
+  const navigate = useNavigate();
 
   const handleSubmit = async () => {
     const profileData = new FormData();
     profileData.append("first_name", firstName);
     profileData.append("last_name", lastName);
+    profileData.append("username", username);
     profileData.append("school", school);
     profileData.append("major", major);
+    profileData.append("header", header);
     profileData.append("bio", bio);
     profileData.append("linkedin", linkedin);
     profileData.append("github", github);
-    if (resume) {
-      profileData.append("resume", resume);
-    }
     if (pfp) {
       profileData.append("profile_picture", pfp);
     }
+    if (banner) {
+      profileData.append("banner", banner);
+    }
+    if (resume) {
+      profileData.append("resume", resume);
+    }
 
     try {
-      const res = await api.post("/api/profiles/private/", profileData, {
-        headers: {
-          "Content-Type": "multipart/form-data",
-        },
-      });
+      const res = await api.post("/api/profiles/private/", profileData);
       console.log(res);
       setUserPfp(res.data.profile_picture);
       navigate("/");
@@ -53,24 +56,42 @@ const Onboard = () => {
     }
   };
 
-  const renderInfoForm = () => {
+  const statusIndicator = () => {
+    const steps = [1, 2];
     return (
-      <div className="w-full">
-        <h2 className="text-2xl font-semibold text-center">
-          Welcome to Parrot
-        </h2>
-        <p className="text-slate-400 text-center mt-2">
-          Lets's start your profile so you can connect with people with cool
-          ideas or share your own!
-        </p>
-        <div className="flex-col mt-4">
-          <PhotoInput pfp={null} setPfp={setPfp} />
-          <p className="text-slate-400 text-center mt-2">
-            Hover over profile to change picture
-          </p>
+      <div className="flex items-center justify-center space-x-4">
+        {steps.map((step) => (
+          <div
+            key={step}
+            className={`w-12 h-12 rounded-full flex items-center justify-center font-bold transition-colors duration-300 ${
+              step <= slide
+                ? "bg-blue-500 text-white"
+                : "bg-gray-200 text-gray-900"
+            }`}
+          >
+            {step}
+          </div>
+        ))}
+      </div>
+    );
+  };
+
+  const firstStage = () => {
+    return (
+      <div className="w-full flex flex-col space-y-2">
+        <div className="w-full">
+          <div className="h-60 w-full object-cover rounded-2xl relative">
+            <div className="h-48 w-full">
+              <BannerInput banner={null} setBanner={setBanner} />
+            </div>
+            <div className="h-40 w-40 rounded-full bg-white  absolute bottom-0 left-12">
+              <ProfilePictureInput pfp={null} setPfp={setPfp} />
+            </div>
+          </div>
         </div>
-        <div className="flex-col mt-4">
-          <h3 className="text-md font-semibold">Your Name *</h3>
+
+        <div className="flex-col">
+          <h3 className="text-sm font-semibold">Your Name *</h3>
           <div className="flex gap-2 my-1">
             <input
               type="text"
@@ -90,6 +111,61 @@ const Onboard = () => {
             />
           </div>
         </div>
+        <div className="flex-col">
+          <h3 className="text-sm font-semibold">Username *</h3>
+          <input
+            type="text"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+            placeholder="@usernmae"
+            className="border border-border rounded-lg p-2 text-sm outline-black w-full"
+          />
+        </div>
+        <div className="flex-col">
+          <h3 className="text-sm font-semibold">Header</h3>
+          <input
+            type="text"
+            value={header}
+            onChange={(e) => setHeader(e.target.value)}
+            placeholder="Computer Science Student @University"
+            className="border border-border rounded-lg p-2 text-sm outline-black w-full"
+          />
+        </div>
+        <div className="flex-col">
+          <h3 className="text-sm font-semibold">Bio</h3>
+          <textarea
+            value={bio}
+            onChange={(e) => setBio(e.target.value)}
+            placeholder="Something About Yourself (Optional)"
+            className="border border-border rounded-lg p-2 text-sm outline-black w-full resize-none"
+          />
+        </div>
+        <Button
+          className="w-full bg-parrot-red text-white hover:bg-black"
+          onClick={() => {
+            if (firstName !== "" && lastName !== "" && username !== "") {
+              setSlide(slide + 1);
+            } else {
+              alert("Please enter first, last name, and username");
+            }
+          }}
+        >
+          Next
+        </Button>
+      </div>
+    );
+  };
+
+  const secondStage = () => {
+    return (
+      <div className="w-full">
+        <Button
+          className="bg-transparent text-black text-md hover:underline p-0"
+          onClick={() => setSlide(slide - 1)}
+        >
+          Back
+        </Button>
+
         <div className="flex-col mt-4">
           <h3 className="txext-md font-semibold"> School</h3>
           <input
@@ -110,44 +186,6 @@ const Onboard = () => {
             className="border border-border rounded-lg p-2 text-sm outline-black w-full"
           />
         </div>
-        <div className="flex-col mt-4">
-          <h3 className="text-md font-semibold">Bio</h3>
-          <textarea
-            value={bio}
-            onChange={(e) => setBio(e.target.value)}
-            placeholder="Something About Yourself (Optional)"
-            className="border border-border rounded-lg p-2 text-sm outline-black w-full"
-          />
-        </div>
-        <Button
-          className="w-full mt-4"
-          onClick={() => {
-            if (firstName !== "" && lastName !== "") {
-              setSlide(slide + 1);
-            } else {
-              alert("Please enter first and last name");
-            }
-          }}
-        >
-          Next
-        </Button>
-      </div>
-    );
-  };
-
-  const renderLinksForm = () => {
-    return (
-      <div className="w-full">
-        <Button
-          className="bg-transparent text-black text-md hover:underline p-0"
-          onClick={() => setSlide(slide - 1)}
-        >
-          Back
-        </Button>
-        <h2 className="text-2xl font-semibold text-center">Additional Links</h2>
-        <p className="text-slate-400 text-center mt-2">
-          Add your resume and other links so people can get to know you better!
-        </p>
         <div className="flex-col mt-4">
           <h3 className="text-md font-semibold">Resume</h3>
           <input
@@ -190,22 +228,14 @@ const Onboard = () => {
 
   return (
     <div className="h-full w-full p-10">
-      <div className="flex gap-2">
-        <img
-          src="../../icon.svg"
-          alt="logo"
-          className="w-8 h-8 object-cover select-none"
-          onClick={() => navigate("/")}
-        />
-        <h1 className="text-[23px] font-medium text-primary lg:block hidden select-none">
-          PARROT
-        </h1>
-      </div>
-
-      <div className="flex items-center justify-center p-10">
+      <div className="flex items-center justify-center">
         <div className="w-[600px]">
-          <div className="mt-16">{slide === 1 && renderInfoForm()}</div>
-          <div className="mt-16 ">{slide === 2 && renderLinksForm()}</div>
+          <h2 className="text-2xl font-semibold text-center">
+            Let's Create Your Parrot Profile
+          </h2>
+          {statusIndicator()}
+          {slide === 1 && firstStage()}
+          {slide === 2 && secondStage()}
         </div>
       </div>
     </div>
